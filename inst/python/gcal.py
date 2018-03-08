4c924d63eb7bc3af402fcfbb02aaa03ee18bcdd0
Olivia Brode-Roger <nibr@mit.edu>
2018-03-08 13:45:29 -0500

    MOre python

--- inst/python/gcal.py
+++ inst/python/gcal.py
@@ -12,6 +12,9 @@ from google.oauth2 import service_account
 
 import datetime
 
+
+lab_calendar = "6.004 Lab hours"
+
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -93,7 +96,7 @@ def get_staff_calendar_id(service, kerberos):
     return new_calendar['id']
 
 
-def update_lab_hours(kerberos, begin, end):
+def lab_hours_add(kerberos, begin, end):
   service = get_calendar_service()
 
   event = {
@@ -126,7 +129,61 @@ def update_lab_hours(kerberos, begin, end):
   lab_id  = get_staff_calendar_id(service, "6.004 Lab hours")
   event = service.events().insert(calendarId=lab_id, body=event).execute()
 
-  print('Event created: %s' % (event.get('htmlLink')))
+
+
+
+def lab_hours_remove(kerberos, begin, end):
+  service = get_calendar_service()
+  lab_id  = get_staff_calendar_id(service, lab_calendar)
+
+  # Remove from staff's calendar
+  cal_id = get_staff_calendar_id(service, kerberos)
+  events = get_lab_hours(service, kerberos)
+
+  for e in events:
+    if e['start']['dateTime'] == begin and e['end']['dateTime'] == end:
+      service.events().delete(calendarId=cal_id, eventId = e['id']).execute()
+
+  # Remove from lab's calendar
+  cal_id = get_staff_calendar_id(service, lab_calendar)
+  events = get_lab_hours(service, lab_calendar)
+  event_title = kerberos + " lab hours"
+
+  for e in events:
+    if e['summary'] == event_title and e['start']['dateTime'] == begin and e['end']['dateTime'] == end:
+      service.events().delete(calendarId=lab_id, eventId = e['id']).execute()
+
+
+
+
+def get_lab_hours(service, kerberos):
+  cal_id = get_staff_calendar_id(service, kerberos)
+
+  page_token = None
+  events = []
+  while True:
+    page = service.events().list(calendarId=cal_id, pageToken=page_token).execute()
+    page_token = page.get('nextPageToken')
+
+    # print('a')
+    # print(events)
+    events.extend(page['items'])
+    # print(events)
+    # print('')
+    if not page_token:
+      break
+
+  # print(kerberos + " events")
+  # for e in events:
+  #   print(e)
+  #   print("")
+  # print("")
+  # print("")
+
+  return events
+
+
+
 
 def main():
   """Shows basic usage of the Google Calendar API.
@@ -135,36 +192,52 @@ def main():
   10 events on the user's calendar.
   """
   service = get_calendar_service()
+  kerberoi = ["nibr", "6.004 Lab hours", "helik"]
 
-  cals = calendar_list(service)
-  for c in cals: print(c['summary'], c['id'])
-
-  print("")
+  print("Calendars")
   nibr_id = get_staff_calendar_id(service, "nibr")
   lab_id  = get_staff_calendar_id(service, "6.004 Lab hours")
-
-  cals = calendar_list(service)
-  for c in cals: print(c['summary'], c['id'])
-
+  for k in kerberoi:
+    print(get_staff_calendar_id(service, k))
   print("")
-  print("----")
-  print("ACL")
 
-  rule = {
-    'role' : "reader",
-    'scope' : {
-      'type': 'default'
-    }
-  }
-  service.acl().insert(calendarId=nibr_id, body = rule).execute()
-  service.acl().insert(calendarId= lab_id, body = rule).execute()
-  acl = service.acl().list(calendarId=nibr_id).execute()
-  for r in acl: print(r, acl[r])
+  lab_hours_remove("helik", "2018-03-08T12:00:00-05:00", "2018-03-08T13:00:00-05:00")
+  lab_hours_add("helik", "2018-03-08T12:00:00-05:00", "2018-03-08T14:30:00-05:00")
 
-  acl = service.acl().list(calendarId= lab_id).execute()
-  for r in acl: print(r, acl[r])
+  print("Events")
+  for k in kerberoi:
+    print(" ", k)
+    events = get_lab_hours(service, k)
+    for e in events:
+      print('  - id', e['id'])
+      print('  - title', e['summary'])
+      print('  - begin ', str(e['start']))
+      print('  - finish',   str(e['end']))
+      # service.events().delete(calendarId=lab_id, eventId = e['id']).execute()
+    print("")
 
-  update_lab_hours("nibr", "2018-03-08T12:00:00-0500", "2018-03-08T13:00:00-0500")
+
+
+
+  # lab_hours_add("nibr", "2018-03-08T12:00:00-0500", "2018-03-08T13:00:00-0500")
+  # print("")
+  # print("----")
+  # print("ACL")
+
+  # rule = {
+    # 'role' : "reader",
+    # 'scope' : {
+      # 'type': 'default'
+    # }
+  # }
+  # service.acl().insert(calendarId=nibr_id, body = rule).execute()
+  # service.acl().insert(calendarId= lab_id, body = rule).execute()
+  # acl = service.acl().list(calendarId=nibr_id).execute()
+  # for r in acl: print(r, acl[r])
+
+  # acl = service.acl().list(calendarId= lab_id).execute()
+  # for r in acl: print(r, acl[r])
+
 
 
 
